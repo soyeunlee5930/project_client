e1216bfa4ccb623dc81583280d1a4616a7a8aee0
soyeunlee <soyeun.lee5930@gmail.com>
2024-05-10 10:57:31 +0900

    feat : router 연결, logout 기능

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import KakaoLogin from './components/KakaoLogin/kakaoLogin';
 import Header from './components/Header/Header';
 
 const Router = () => {
+  const isLogin = !!localStorage.getItem('accessToken');
+
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login/kakao" element={<KakaoLogin />} />
+        <Route
+          path="/login"
+          element={isLogin ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/oauth2/login/kakao"
+          element={isLogin ? <Navigate to="/" /> : <KakaoLogin />}
+        />
         <Route path="/header" element={<Header />} />
       </Routes>
     </BrowserRouter>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,47 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.scss';
 
 const Header = () => {
   const isLogin = !!localStorage.getItem('accessToken');
   const accessToken = localStorage.getItem('accessToken');
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    fetch(`${process.env.REACT_APP_LOGOUT_URL_SERVER}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: accessToken,
+      },
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch');
+        }
+        return res.json;
+      })
+      .then(data => {
+        localStorage.removeItem('accessToken');
+        alert('로그아웃되었습니다.');
+        navigate('/');
+        window.location.reload();
+      });
+  };
 
   return (
     <div className="header">
       <div className="logoImg">
         <img src="/images/logo2_small.png" alt="로고" />
+        {isLogin ? (
+          <button className="handleLogoutBtn" onClick={handleLogout}>
+            로그아웃
+          </button>
+        ) : (
+          <button className="handleLoginBtn" onClick={() => navigate('/login')}>
+            로그인
+          </button>
+        )}
       </div>
     </div>
   );
